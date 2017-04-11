74cc72515ecd9c60e7c6777e61895d98d3eea306
Gregory <gregpappas9@gmail.com>
2017-04-11 18:55:20 -0500

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+const request = require("request-promise-native");
+
+exports.dog = function () {
+    return new Promise((resolve, reject) => {
+        request("https://random.dog/woof").then(JSON.stringify).then(JSON.parse).then(dog => resolve(`https://random.dog/${dog.lastIndexOf(".") === "mp4" ? "8536-28743-5665.jpg" : dog}`))
+        .catch(err => reject(Error(err)));
+    });
+};
+
+exports.cat = function () {
+    return new Promise((resolve, reject) => {
+        request("https://random.cat/meow").then(JSON.parse).then(cat => resolve(cat.file))
+        .catch(err => reject(Error(err)));
+    });
+};
