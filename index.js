c06451cce1ae5e4127fdb1d79fbd4828d61fd126
Gregory <gregpappas9@gmail.com>
2017-04-11 20:01:31 -0500

    :eyes:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require("request-promise-native");
 
 exports.dog = function () {
     return new Promise((resolve, reject) => {
-        request("https://random.dog/woof").then(JSON.stringify).then(JSON.parse).then(dog => resolve(`https://random.dog/${dog.lastIndexOf(".") === ".mp4" ? "8536-28743-5665.jpg" : dog}`))
+        request("https://random.dog/woof").then(JSON.stringify).then(JSON.parse).then(dog => resolve(`https://random.dog/${dog.includes(".mp4") ? "8536-28743-5665.jpg" : dog}`))
         .catch(err => reject(Error(err)));
     });
 };
